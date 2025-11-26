9410cd4216a540f972ef8eda33458c61a8e5f57a
ella <lydbsdud@gmail.com>
2025-11-26 21:19:46 +0900

    feat: 기본 구조 설정

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPage from "./features/auth/routes/login-page";
+
+function App() {
+  return (
+    <div style={{ height: "100%" }}>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
